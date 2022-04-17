05a997f8055deb3c88325087a67e264a407770af
Azarill <heierms@gmail.com>
2022-04-16 21:16:22 -0500

    added fetch for user data on github api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,24 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user){
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+
+
+
+        })
+
+
+
+
+    });
 
 }
 
+getUserRepos("Azarill");
+getUserRepos("microsoft");
+getUserRepos("Azariill");0
 
-getUserRepos();
