759ad847443b44312b8d807a9d82bfe6c913e5f4
Azarill <heierms@gmail.com>
2022-04-20 16:00:49 -0500

    added language button sorting

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-containers");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user){
     // format the github api url
@@ -102,8 +103,34 @@ var displayRepos= function(repos, searchTerm){
     }
 }
 
+var getFeaturedRepos = function(language){
+  
+  var apiUrl = `https://api.github.com/search/repositories?q=${language}+is:featured&sort=help-wanted-issues`;
+
+  fetch(apiUrl).then(function(response){
+    if(response.ok){
+      response.json().then(function(data){
+        displayRepos(data.items, language);
+      })
+    }else{
+      alert("Error: GitHub User Not Found");
+    }
+  });
+}
+
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+
+  if(language){
+  getFeaturedRepos(language);
+
+  repoContainerEl.textContent="";
+  }
+
+}
 
 
 
 
+languageButtonEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit" , formSubmitHandler);
